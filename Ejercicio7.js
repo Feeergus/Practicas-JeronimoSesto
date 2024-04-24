e61b8e7f6c234823736bde18495afb38f11e2301
Jeronimo Sesto <109633243+Feeergus@users.noreply.github.com>
2024-04-24 15:34:53 -0300

    Cmabio

---
+++ Ejercicio7.js
@@ -0,0 +1,12 @@
+let Valor1 = 1
+let Valor2 = 6
+
+if(Valor1 < Valor2){
+    let difValor = Valor2 - 1
+    let difInicio = Valor1 + 1
+    let mulTres = 
+    for(let i = difInicio; i <= difValor;i++){
+        
+        console.log(i)
+    }
+}
